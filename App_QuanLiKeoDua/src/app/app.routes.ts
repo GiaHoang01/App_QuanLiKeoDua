8da5ded262be2a0bb168c709f32056d764f167ec
GiaHoang01 <gianh1906@gmail.com>
2024-11-27 14:51:22 +0700

    commit

--- App_QuanLiKeoDua/src/app/app.routes.ts
+++ App_QuanLiKeoDua/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { DefaultLayoutComponent } from './layout';
+import { PermissionGuard } from '../scss/services/guard.service';
 export const routes: Routes = [
   {
     path: '',
@@ -10,47 +11,64 @@ export const routes: Routes = [
     path: '',
     component: DefaultLayoutComponent,
     data: {
-      title: 'Home'
+        title: 'Home',
+        data: { requiredPermission: 'Xem phiếu nhập hàng' },
+        canActivate: [PermissionGuard],
     },
     children: [
       {
         path: 'pages',
-        loadChildren: () => import('./views/pages/routes').then((m) => m.routes)
+        loadChildren: () => import('./views/pages/routes').then((m) => m.routes),
+         data: { requiredPermission: 'Xem phiếu nhập hàng' },
+        canActivate: [PermissionGuard],
       },
       {
         path: 'accounts',
-        loadChildren: () => import('./views/accounts/routes').then((m) => m.routes)
+        loadChildren: () => import('./views/accounts/routes').then((m) => m.routes),
+        data: { requiredPermission: 'Xem phiếu nhập hàng' },
+        canActivate: [PermissionGuard],
       },
       {
         path: 'saleorder',
-        data: {
-          title: 'Đơn đặt hàng'
-        },
+        data: { requiredPermission: 'Xem phiếu nhập hàng' },
+        canActivate: [PermissionGuard],
         loadChildren: () => import('./views/sale/routes').then((m) => m.routes)
       },
       {
         path: 'employee',
-        loadChildren: () => import('./views/employees/routes').then((m) => m.routes)
+        loadChildren: () => import('./views/employees/routes').then((m) => m.routes),
+        data: { requiredPermission: 'Xem phiếu nhập hàng' },
+        canActivate: [PermissionGuard],
       },
       {
         path: '',
-        loadChildren: () => import('./views/purchase/routes').then((m) => m.routes)
+        loadChildren: () => import('./views/purchase/routes').then((m) => m.routes),
+        data: { requiredPermission: 'Xem phiếu nhập hàng' },
+        canActivate: [PermissionGuard],
       },
       {
         path: 'product',
-        loadChildren: () => import('./views/products/routes').then((m) => m.routes)
+        loadChildren: () => import('./views/products/routes').then((m) => m.routes),
+        data: { requiredPermission: 'VIEW_SALE_ORDER' },
+        canActivate: [PermissionGuard],
       },
       {
         path: 'customer',
-        loadChildren: () => import('./views/customers/routes').then((m) => m.routes)
+        loadChildren: () => import('./views/customers/routes').then((m) => m.routes),
+        data: { requiredPermission: 'VIEW_SALE_ORDER' },
+        canActivate: [PermissionGuard],
       },
       {
         path: '',
-        loadChildren: () => import('./views/sale/routes').then((m) => m.routes)
+        loadChildren: () => import('./views/sale/routes').then((m) => m.routes),
+        data: { requiredPermission: 'VIEW_SALE_ORDER' },
+        canActivate: [PermissionGuard],
       },
       {
         path: '',
-        loadChildren: () => import('./views/shipping/routes').then((m) => m.routes)
+        loadChildren: () => import('./views/shipping/routes').then((m) => m.routes),
+        data: { requiredPermission: 'VIEW_SALE_ORDER' },
+        canActivate: [PermissionGuard],
       }, 
     ]
   },
--- App_QuanLiKeoDua/src/app/layout/default-layout/_nav.ts
+++ App_QuanLiKeoDua/src/app/layout/default-layout/_nav.ts
@@ -1,83 +1,92 @@
 import { INavData } from '@coreui/angular';
-
 export const navItems: INavData[] = [
   {
     name: 'Danh mục',
     url: '',
-    icon:"bi bi-list-task",
+    icon: "bi bi-list-task",
     children: [
       {
         name: 'Nhân viên',
         url: '/employee',
         icon: '',
+        permission: 'Xem nhân viên',
       },
       {
         name: 'Hàng hóa',
         url: '/product',
         icon: '',
+        permission: 'VIEW_SALE_ORDER',
       },
       {
         name: 'Khách hàng',
         url: '/customer',
         icon: '',
+        permission: 'VIEW_SALE_ORDER',
       }
     ]
   },
   {
     name: 'Bán hàng',
     url: '',
-    icon:"bi bi-basket2-fill",
-    children:[
+    icon: "bi bi-basket2-fill",
+    children: [
       {
-        name:'Đơn đặt hàng',
-        url:'/saleorder',
-        icon:''
+        name: 'Đơn đặt hàng',
+        url: '/saleorder',
+        icon: '',
+        permission: 'VIEW_SALE_ORDER',
       },
       {
-        name:'Duyệt đơn hàng',
-        url:'/confirmsaleorder',
-        icon:''
+        name: 'Duyệt đơn hàng',
+        url: '/confirmsaleorder',
+        icon: '',
+        permission: 'VIEW_SALE_ORDER', 
       }
     ]
   },
   {
     name: 'Nhập hàng',
     url: '',
-    icon:"bi bi-box-seam-fill",
-    children:[
+    icon: "bi bi-box-seam-fill",
+    children: [
       {
-        name:'Yêu cầu nhập hàng',
-        url:'/purchaseOrderRequest',
-        icon:''
+        name: 'Yêu cầu nhập hàng',
+        url: '/purchaseOrderRequest',
+        icon: '',
+        permission: 'Xem phiếu nhập hàng', 
       },
       {
-        name:'Phiếu nhập hàng',
-        url:'/purchaseOrder',
-        icon:''
+        name: 'Phiếu nhập hàng',
+        url: '/purchaseOrder',
+        icon: '',
+        permission: 'Xem phiếu nhập hàng',
       },
       {
-        name:'Duyệt phiếu nhập',
-        url:'/confirmPurchaseOrder',
-        icon:''
+        name: 'Duyệt phiếu nhập',
+        url: '/confirmPurchaseOrder',
+        icon: '',
+        permission: 'VIEW_SALE_ORDER', 
       },
     ]
   },
   {
     name: 'Quản lí kho',
     url: '',
-    icon:"bi bi-database-fill",
-    children:[
+    icon: "bi bi-database-fill",
+    children: [
       {
-        name:'Phiếu giao hàng',
-        url:'/shippingnote',
-        icon:''
+        name: 'Phiếu giao hàng',
+        url: '/shippingnote',
+        icon: '',
+        permission: 'VIEW_SALE_ORDER', 
       }
     ]
   },
   {
     name: 'Tài khoản',
     url: '/accounts',
-    icon:"bi bi-person-fill",
+    icon: "bi bi-person-fill",
+    permission: 'VIEW_SALE_ORDER',
   }
- 
 ];
+// để ẩn menu trên app
